8e6870266a289d77d64a457d38bc57e54fb5e13e
Josh Stolberg <stolbejj@gmail.com>
2018-01-21 14:30:23 -0600

    Instafuel Mods

--- termsandconditions/static/termsandconditions/js/modal.js
+++ termsandconditions/static/termsandconditions/js/modal.js
@@ -4,4 +4,14 @@ function termsandconditions_overlay() {
     for (i = 0; i < el.length; i++) {
         el[i].style.visibility = (el[i].style.visibility == "visible") ? "hidden" : "visible";
     };
+     $("body").toggleClass("dialogIsOpen");
+     window.onkeyup = function(e) {
+       var key = e.keyCode ? e.keyCode : e.which;
+
+       if (key == 38) {
+           playerSpriteX += 10;
+       }else if (key == 40) {
+           playerSpriteX -= 10;
+       }
+    }
 };

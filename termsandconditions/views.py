00060a35213c081cbafb4bea9939fe2aa1b1be47
Josh Stolberg <stolbejj@gmail.com>
2018-02-06 15:57:18 -0600

    adding signed as to model and adjusting accept modal

--- termsandconditions/views.py
+++ termsandconditions/views.py
@@ -125,7 +125,7 @@ class AcceptTermsView(CreateView, GetTermsViewMixin):
                     user=user,
                     terms=TermsAndConditions.objects.get(pk=int(terms_id)),
                     ip_address=ip_address,
-                    signed_as=request.POST.get('accept','')
+                    signed_as=request.POST.get('name','')
                 )
                 new_user_terms.save()
             except IntegrityError:  # pragma: nocover

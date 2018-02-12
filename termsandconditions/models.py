ed4da88bdf530c40d85387e27ebfef960d4b4886
Josh Stolberg <stolbejj@gmail.com>
2018-02-12 16:26:50 -0600

    one company user signing the doc means that no other company users have to sign it

--- termsandconditions/models.py
+++ termsandconditions/models.py
@@ -8,8 +8,7 @@ from django.conf import settings
 from django.utils import timezone
 from django.core.cache import cache
 import logging
-#from accounts.models import FleetCompanyUser
-from fleets.models import FleetCompany
+from fleets.utils import is_company_signed
 
 LOGGER = logging.getLogger(name='termsandconditions')
 
@@ -117,9 +116,10 @@ class TermsAndConditions(models.Model):
     @staticmethod
     def get_active_terms_not_agreed_to(user):
         """Checks to see if a specified user has agreed to all the latest terms and conditions"""
-        #user = FleetCompanyUser.objects.filter(pk=user.id)
-        if user.company not in FleetCompany.objects.companies_not_signed_tc():
+
+		if is_company_signed(user):
             return []
+        #if company has signed, return blank
 		
         if TERMS_EXCLUDE_USERS_WITH_PERM is not None:
             if user.has_perm(TERMS_EXCLUDE_USERS_WITH_PERM) and not user.is_superuser:
